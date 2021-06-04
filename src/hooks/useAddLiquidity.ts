31cdc6d5c9eb014bbcc70ca3f1d3ce20fd6e8e00
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-06-04 18:45:09 +0200

    Cleanup hook

--- src/hooks/useAddLiquidity.ts
+++ src/hooks/useAddLiquidity.ts
@@ -27,41 +27,6 @@ const useAddLiquidity = (bank: Bank) => {
     },
     [bank, antToken],
   );
-
- /* const handleAddLiquidity = useCallback(
-    async (amount0: number, amount1: number, token0In: boolean) => {
-        const token0 = antToken.tokens[bank.token0Name];
-        const token1 = antToken.tokens[bank.token1Name];
-        
-        const liquidityHelper = antToken.contracts[bank.providerHelperName];
-
-        const [reserve0, reserve1 ] = await antToken.getPairReserves(bank);
-
-        if (token0In)
-        {
-          const token0Amount = parseUnits(amount0.toString(), token0.decimal);
-          const token1Amount = token0Amount.mul(reserve1).div(reserve0);
-
-          handleTransactionReceipt(
-            liquidityHelper.stake(token0Amount, token1Amount, 0, 0, deadline()),
-            `Adding ${amount0}/${amount1} ${bank.token0Name}/${bank.token1Name} to liquidity pool`,
-          );
-        }
-        else
-        {
-          const token1Amount = parseUnits(amount1.toString(), token1.decimal);
-          const token0Amount = token1Amount.mul(reserve0).div(reserve1);
-
-          handleTransactionReceipt(
-            liquidityHelper.stake(token0Amount, token1Amount, 0, 0, deadline()),
-            `Adding ${amount0}/${amount1} ${bank.token0Name}/${bank.token1Name} to liquidity pool`,
-          );
-        }
-
-        
-    },
-    [bank, antToken],
-  );*/
   return { onAddLiquidity: handleAddLiquidity };
 };
 
