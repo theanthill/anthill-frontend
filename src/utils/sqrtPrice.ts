d334f6d52e438b445b3e13b390393fa45dc47bb5
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-09-08 11:16:13 +0200

    Fix price calculation adding decimals

--- src/utils/sqrtPrice.ts
+++ src/utils/sqrtPrice.ts
@@ -1,9 +1,9 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
 
-export function decodeSqrtX96(sqrtPriceX96: BigNumber) : BigNumber {
-
-    const priceSquare = sqrtPriceX96.mul(sqrtPriceX96);
-    const shiftAmount = BigNumber.from(2).pow(192);
-
-    return priceSquare.div(shiftAmount);
+export function decodeSqrtX96(sqrtPriceX96: BigNumber, decimals: number): number {
+  const priceSquare = sqrtPriceX96.mul(sqrtPriceX96);
+  const shiftAmount = BigNumber.from(2).pow(192);
+  const result = priceSquare.mul(10 ** decimals).div(shiftAmount);
+  return Number(formatUnits(result, decimals));
 }
