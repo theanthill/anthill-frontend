dd3a4b01e4439afe13aad1292a72db9882abcf32
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-09-09 20:08:49 +0200

    Fixed website build

--- src/utils/mostSignificantBit.js
+++ src/utils/mostSignificantBit.js
@@ -2,22 +2,21 @@ const JSBI = require('jsbi');
 //import invariant from 'tiny-invariant'
 
 const TWO = JSBI.BigInt(2);
-const POWERS_OF_2 = [128, 64, 32, 16, 8, 4, 2, 1].map((pow) => [pow, JSBI.exponentiate(TWO, JSBI.BigInt(pow))]);
+const POWERS_OF_2 = [128, 64, 32, 16, 8, 4, 2, 1].map((pow) => [
+  pow,
+  JSBI.exponentiate(TWO, JSBI.BigInt(pow)),
+]);
 
-function mostSignificantBit(x) {
-    //invariant(JSBI.greaterThan(x, ZERO), 'ZERO')
-    //invariant(JSBI.lessThanOrEqual(x, MaxUint256), 'MAX')
+export function mostSignificantBit(x) {
+  //invariant(JSBI.greaterThan(x, ZERO), 'ZERO')
+  //invariant(JSBI.lessThanOrEqual(x, MaxUint256), 'MAX')
 
-    let msb = 0;
-    for (const [power, min] of POWERS_OF_2) {
-        if (JSBI.greaterThanOrEqual(x, min)) {
-            x = JSBI.signedRightShift(x, JSBI.BigInt(power));
-            msb += power;
-        }
+  let msb = 0;
+  for (const [power, min] of POWERS_OF_2) {
+    if (JSBI.greaterThanOrEqual(x, min)) {
+      x = JSBI.signedRightShift(x, JSBI.BigInt(power));
+      msb += power;
     }
-    return msb;
+  }
+  return msb;
 }
-
-module.exports = {
-    mostSignificantBit,
-};
